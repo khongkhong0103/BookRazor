6f9f76b3b4942a6c2efe65e9e4a7166b542ef94d
Trong Tam <trongtam068@gmail.com>
2023-11-06 22:22:51 +0700

    models & Interface

---
+++ Interface/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookRazor.Models;
+using System.Collections.Generic;
+
+namespace BookRazor.Interface
+{
+	public interface IRepository
+	{
+		public HashSet<Book> Books { get; set; }
+		public Book Get(int id);
+	}
+}
---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace BookRazor.Models
+{
+	public class Book
+	{
+		public int Id { get; set; }
+		public string Title { get; set; } = "A new book";
+		public string Authors { get; set; } = "Authors";
+		public string Publisher { get; set; } = "Publisher";
+		public int Year { get; set; } = DateTime.Now.Year;
+		public string Description { get; set; } = "";
+	}
+}
