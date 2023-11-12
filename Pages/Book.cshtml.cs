bbe2a0b26ffc4e4804de1eb39948e6a6c86edbc7
Trong Tam <trongtam068@gmail.com>
2023-11-13 03:09:34 +0700

    book

---
+++ Pages/Book.cshtml.cs
@@ -0,0 +1,22 @@
+using BookRazor.Interface;
+using BookRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookRazor.Pages
+{
+    public class BookModel : PageModel
+    {
+        public enum Action { Detail, Delete, Update, Create}
+        private readonly IRepository _repository;
+        public BookModel(IRepository repository) => _repository = repository;
+        public Action Job { get; private set; }
+        public Book Book { get; private set; }
+        public void OnGet(int id)
+        {
+            Job = Action.Detail;
+            Book = _repository.Get(id);
+            ViewData["Title"] = Book == null ? "Book not found !" : $"Detail - {Book.Title}";
+        }
+    }
+}
